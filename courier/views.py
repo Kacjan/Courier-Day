9ac0afb6748d3109671abb27b735d1c5ae591cfe
Kacper Janczyński <kacperjanczynski@gmail.com>
2023-10-21 11:15:06 +0200

    Created Home site

--- courier/views.py
+++ courier/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 from django.views import View
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, TemplateView
 from django.urls import reverse_lazy
 
 from .forms import CourierDayModelForm
@@ -16,18 +16,20 @@ class CourierDayCreateView(CreateView):
     form_class = CourierDayModelForm
     success_url = reverse_lazy('courier_day_list_view')
 
-# def movies_list(request):
-#     return render(
-#         request,
-#         template_name='movies.html',
-#         context={'movies': Movie.objects.all()}
-#     )
-
-
 class CourierDayListView(View):
     def get(self, request):
         return render(
             request,
             template_name='courier_day_list_view.html',
             context={'days': CourierDay.objects.all() }
+        )
+
+# class Home(TemplateView):
+#     template_name = 'home.html'
+
+class Home(View):
+    def get(self, request):
+        return render(
+            request,
+            template_name='home.html'
         )
