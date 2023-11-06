145c76052fab6db3d100f77d62e9d555a558c2ed
Kacper Janczyński <kacperjanczynski@gmail.com>
2023-11-07 00:21:10 +0100

    Changes in forms, added some tests

--- tests/test_urls.py
+++
@@ -1,9 +0,0 @@
-from django.test import SimpleTestCase
-from django.urls import reverse, resolve
-from courier.views import Home, CourierDayCreateView
-class TestUrls(SimpleTestCase):
-
-    def test_home_url_resolves(self):
-        url = reverse('home')
-        print(resolve(url))
-        self.assertEquals(resolve(url).func.view_class, Home)
